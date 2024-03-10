e672cf11ba3dd9bb1fce7ccfcd694b9552033835
LAKSHYA SINGH <92517597+Lakshya-GG@users.noreply.github.com>
2024-03-10 19:35:06 +0530

    update : webhook status

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run PES2UG21CS252\n";
-  court << "WebHook Built and Run\n";
+  court << "WebHook Built and Run TEST 1\n";
   return 0;
 }
